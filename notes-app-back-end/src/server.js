83c26440e628ceafb9e7901d6bf3685cb14ef8ce
Fadhil <fadhil.muq@gmail.com>
2023-02-26 08:54:58 +0700

    add notes

--- notes-app-back-end/src/server.js
+++ notes-app-back-end/src/server.js
@@ -1,1 +1,18 @@
 console.log('Hallo kita akan membuat RESTful API');
+
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 8000,
+        host: 'localhost',
+    });
+
+    server.route(routes);
+
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();

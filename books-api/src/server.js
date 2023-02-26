c30fa7a1d8ef9f3cbbce20d78066965550e23623
Fadhil <fadhil.muq@gmail.com>
2023-02-26 23:01:59 +0700

    submission API bookshelf

---
+++ books-api/src/server.js
@@ -0,0 +1,18 @@
+console.log('Bookshelf RESTful API');
+
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: 'localhost',
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();

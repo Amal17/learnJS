e72c60935133b635e90933e36fbfb1ebc50b12d0
Fadhil <fadhil.muq@gmail.com>
2023-02-26 09:14:57 +0700

    get note by id

--- notes-app-back-end/src/routes.js
+++ notes-app-back-end/src/routes.js
@@ -1,4 +1,4 @@
-const { addNoteHandler, getAllNotesHandler } = require('./handlers');
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handlers');
 
 const routes = [
     {
@@ -10,6 +10,11 @@ const routes = [
       method: 'GET',
       path: '/notes',
       handler: getAllNotesHandler,
+    },
+    {
+      method: 'GET',
+      path: '/notes/{id}',
+      handler: getNoteByIdHandler
     },
   ];
    

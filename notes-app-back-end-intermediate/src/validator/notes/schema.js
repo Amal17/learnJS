09cfcc3f8c17929739a8d1ec439a98d8d4e50435
Fadhil <fadhil.muq@gmail.com>
2023-06-14 17:42:40 +0700

    user registration implemented

--- notes-app-back-end-intermediate/src/validator/notes/schema.js
+++ notes-app-back-end-intermediate/src/validator/notes/schema.js
@@ -1,10 +1,10 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const Joi = require('joi');
+const Joi = require('joi')
 
 const NotePayloadSchema = Joi.object({
   title: Joi.string().required(),
   body: Joi.string().required(),
-  tags: Joi.array().items(Joi.string()).required(),
-});
+  tags: Joi.array().items(Joi.string()).required()
+})
 
-module.exports = { NotePayloadSchema };
+module.exports = { NotePayloadSchema }

015faa4dd07898f9bb5023787c2153a6554c3a54
Fadhil <fadhil.muq@gmail.com>
2023-06-22 16:53:59 +0700

    sub 2 - user and auth

--- open-music-api/src/server.js
+++ open-music-api/src/server.js
@@ -1,17 +1,36 @@
 require('dotenv').config()
 
 const Hapi = require('@hapi/hapi')
+// Albums
 const albums = require('./api/albums')
-const songs = require('./api/songs')
 const AlbumsService = require('./services/postgres/AlbumsService')
-const SongsService = require('./services/postgres/SongsService')
 const AlbumsValidator = require('./validator/albums')
+
+// Songs
+const songs = require('./api/songs')
+const SongsService = require('./services/postgres/SongsService')
 const SongsValidator = require('./validator/songs')
+
+// Users
+const users = require('./api/users')
+const UsersService = require('./services/postgres/UsersService')
+const UsersValidator = require('./validator/users')
+
+// Authentications
+const authentications = require('./api/authentications')
+const AuthenticationsService = require('./services/postgres/AuthenticationsService')
+const TokenManager = require('./tokenize/TokenManager')
+const AuthenticationsValidator = require('./validator/authentications')
+
+// Error
 const ClientError = require('./exceptions/ClientError')
 
 const init = async () => {
   const albumsService = new AlbumsService()
   const songsService = new SongsService()
+  const usersService = new UsersService()
+  const authenticationsService = new AuthenticationsService()
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -65,6 +84,20 @@ const init = async () => {
     options: {
       service: songsService,
       validator: SongsValidator
+    }
+  }, {
+    plugin: users,
+    options: {
+      service: usersService,
+      validator: UsersValidator
+    }
+  }, {
+    plugin: authentications,
+    options: {
+      authenticationsService,
+      usersService,
+      tokenManager: TokenManager,
+      validator: AuthenticationsValidator
     }
   }])
 

4dccbc9cc0967c3197f08406e2a17f079990ff12
Fadhil <fadhil.muq@gmail.com>
2023-06-25 13:21:35 +0700

    consumer export playlist

---
+++ open-music-consumer/scr/PlaylistService.js
@@ -0,0 +1,36 @@
+const { Pool } = require('pg')
+ 
+class PlaylistService {
+  constructor() {
+    this._pool = new Pool()
+  }
+
+  async getPlaylistById (id) {
+    const query = {
+      text: `SELECT playlists.id, playlists.name 
+              FROM playlists
+              WHERE playlists.id = $1`,
+      values: [id]
+    }
+    const result = await this._pool.query(query)
+
+    return result.rows[0]
+  }
+
+  async getSongsOnPlaylist (id) {
+    const playlist = await this.getPlaylistById(id)
+
+    const query = {
+      text: `SELECT songs.id, songs.title, songs.performer
+              FROM playlist_songs
+              LEFT JOIN songs on songs.id = playlist_songs.song_id
+              WHERE playlist_id = $1`,
+      values: [id]
+    }
+    const songs = await this._pool.query(query)
+    playlist.songs = songs.rows
+    return playlist
+  }
+}
+
+module.exports = PlaylistService
